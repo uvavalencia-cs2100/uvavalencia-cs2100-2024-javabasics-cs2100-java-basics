5bf6872cea09e181518d9e19e09fb18cdf6b747b
ausias-armesto <ausiasarmesto@gmail.com>
2024-09-29 11:49:47 +0200

    LinkedLists

---
+++ linkedlists/Main.java
@@ -0,0 +1,22 @@
+package linkedlists;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        // Sample usage
+        LinkedList<String> list = new LinkedList<>();
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Cherry");
+        
+        System.out.println("Initial list: " + list);
+        
+        list.remove("Banana");
+        System.out.println("After removing 'Banana': " + list);
+        
+        System.out.println("Does the list contain 'Cherry'? " + list.contains("Cherry"));
+        System.out.println("Does the list contain 'Orange'? " + list.contains("Orange"));
+        System.out.println("Size of the list: " + list.size());
+
+    }
+}
